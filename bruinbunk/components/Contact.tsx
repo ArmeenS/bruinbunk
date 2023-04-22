e479649122b8d95ae2411bfe0281889efade792d
houserjohn <john@g.ucla.edu>
2023-04-22 14:46:31 -0700

    Bruinbunk email set up and displayed on the website

--- bruinbunk/components/Contact.tsx
+++ bruinbunk/components/Contact.tsx
@@ -7,12 +7,14 @@ function Contact() {
             <div id={"contact"} className="flex flex-wrap mt-5 gap-6">
                 <div className="inline-block w-80">
                     <div className="text-white font-medium text-sm">Email</div>
-                    <div className="text-white">lujaynmegally1@ucla.edu</div>
+                    <div className="text-white">bruin@bruinbunk.com</div>
                 </div>
+                {/*
                 <div className="inline-block w-80">
                     <div className="text-white font-medium text-sm">Phone</div>
                     <div className="text-white">(559) 355-4751</div>
                 </div>
+                */}
             </div>
         </div>
     );
