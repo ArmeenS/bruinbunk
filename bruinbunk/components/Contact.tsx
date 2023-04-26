f8fe3260302d5843129aaceaae94fd9b0c829531
Armeen Seyed Makki <armeensm10@g.ucla.edu>
2023-04-25 20:24:07 -0700

    fixed size and spacing of text, fixed footer, added change of colour when hovering over buttons

--- bruinbunk/components/Contact.tsx
+++ bruinbunk/components/Contact.tsx
@@ -1,13 +1,12 @@
 function Contact() {
     return (
         <div className="">
-            <div className="text-white text-4xl font-medium mb-4">
+            <div className="text-white text-4xl font-bold mb-1 pt-8">
                 Contact
             </div>
-            <div id={"contact"} className="flex flex-wrap mt-5 gap-6">
+            <div id={"contact"} className="flex flex-wrap mt-10 gap-6">
                 <div className="inline-block w-80">
-                    <div className="text-white font-medium text-sm">Email</div>
-                    <div className="text-white">bruin@bruinbunk.com</div>
+                    <div className="text-white font-medium mb-6">Email: bruin@bruinbunk.com</div>
                 </div>
                 {/*
                 <div className="inline-block w-80">
--- bruinbunk/components/Landing.tsx
+++ bruinbunk/components/Landing.tsx
@@ -25,7 +25,7 @@ const externalImageLoader = ({ src }: { src: string }) =>
   `https://BruinBunk.com/${src}`;
 
 const EmailSignupSchema = Yup.object().shape({
-    email: Yup.string().email('Invalid email').required('Invalid email'),
+    email: Yup.string().email('Invalid Email').required('Invalid Email'),
 });
 
 interface EmailSignUpData {
@@ -71,14 +71,14 @@ function Portfolio() {
                                     
                                 </div>
                                 
-                                <div className="text-3xl md:text-6xl font-medium mb-4  text-center ">Find your perfect UCLA sublet</div>
-                                <div className="text-3xl md:text-3xl font-medium mb-2  text-center mt-4 ">Get sublet listings</div>
-                                <div className="text-md md:text-md font-medium mb-2 text-center ">Sign up for frequent updates</div>
+                                <div className="text-3xl sm:pt-0 md:pt-14 md:text-7xl font-bold sm:mb-2 md:mb-6  text-center ">Find your perfect UCLA sublet</div>
+                                <div className="text-3xl md:text-4xl font-bold mb-2  text-center mt-4 ">Get sublet listings</div>
+                                <div className="text-md md:text-md font-medium mb-0 text-center ">Sign up for frequent updates!</div>
                             </div>
                         </div>
                         <div className="w-full pb-10">
                             <div className="flex justify-center m-auto ">
-                                <div id={"about"} className="text-center max-w-7xl flex flex-col items-center">
+                                <div id={"about"} className="text-center max-w-87l flex flex-col items-center">
                                     <div className="w-full">
                                         <div>
                                             <div className="w-full">                                        
@@ -96,9 +96,9 @@ function Portfolio() {
                                                     >
                                                         {({ isSubmitting }) => (
                                                             <Form>
-                                                                <Field placeholder="Email" className="w-fit bg-background py-2 font-medium inline-block border border-black rounded-full pl-4 pr-12 select-none focus:outline-none py-1" type="email" name="email" />
-                                                                <button className="font-medium w-fit px-6 bg-bruinblue rounded-full py-2 text-gray-100 -ml-10 border border-bruinblue" type="submit" disabled={isSubmitting}>
-                                                                    Sign up
+                                                                <Field placeholder="Email" className="w-fit bg-background py-2 font-medium inline-block border border-black rounded-full pl-4 pr-12 select-none focus:outline-none py-1 md:text-3xl" type="email" name="email" />
+                                                                <button className="font-medium w-fit px-6 bg-bruinblue rounded-full py-2 text-gray-100 -ml-20 border border-bruinblue md:text-3xl hover:bg-green-600 hover:border-green-600" type="submit" disabled={isSubmitting}>
+                                                                    Sign Up
                                                                 </button>
                                                                 <ErrorMessage className="text-red-600 text-xs" name="email" component="div" />
 
@@ -110,17 +110,17 @@ function Portfolio() {
                                                 
                                             </div>
                                             
-                                            <div className="font-medium text-3xl mb-4 mt-4">
+                                            <div className="font-bold text-3xl mb-4 mt-4 pt-4">
                                                 List your sublet 
                                             </div>
                                             <div className="m-auto w-fit">
-                                                <a className="text-white font-medium bg-bruinblue w-fit rounded-full px-4 py-2 flex flex-row " href="https://docs.google.com/forms/d/e/1FAIpQLScIaUS8PM8nP6N0DbzC25FR3rpqiM_aVlUX3dxtFq2R1yaBiw/viewform?usp=sf_link">
+                                                <a className="text-white font-medium bg-bruinblue w-fit rounded-full px-4 py-2 flex flex-row md:text-3xl hover:bg-green-600" href="https://docs.google.com/forms/d/e/1FAIpQLScIaUS8PM8nP6N0DbzC25FR3rpqiM_aVlUX3dxtFq2R1yaBiw/viewform?usp=sf_link">
                                                     <div className="">
                                                         Enter your information here
 
                                                     </div>
                                                     {/* Center the arrow vertically */}
-                                                    <div className="ml-1 flex bg-bruinblue">
+                                                    <div className="ml-1 flex">
                                                         <div className="m-auto">
                                                             <BsArrowRight className=""/>
                                                         </div>
@@ -143,17 +143,15 @@ function Portfolio() {
                             
                             <Image src="BruinBunkSublet.svg" width={750} height={750} loader={externalImageLoader} alt="Bruin Bunk"  className=""/>
                             
-                            {/* 
-                            <Image src={BruinBunkSublet} width={750} height={750} alt="Bruin Bunk"  className=""/>
-                            */}
+                             
+                            {/*<Image src={BruinBunkSublet} width={750} height={750} alt="Bruin Bunk"  className=""/>*/}
+                            
                         </div>
                         <div className="md:hidden m-auto w-fit">
                             
                             <Image src="BruinBunkSublet.svg" width={300} height={300} loader={externalImageLoader} alt="Bruin Bunk"  className=""/>
                             
-                            {/*
-                            <Image src={BruinBunkSublet} width={300} height={300} alt="Bruin Bunk"  className=""/>
-                            */}
+                            {/*<Image src={BruinBunkSublet} width={300} height={300} alt="Bruin Bunk"  className=""/>*/}
                         </div>
                         
                     </div>
